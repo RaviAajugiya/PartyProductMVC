c9f2b87386a73f4968bae72c27b7d0a39640eee4
Ravi Aajugiya <raviaajugiya@weybee.in>
2023-11-06 16:01:30 +0530

    Add project files.

---
+++ Migrations/202311061024352_AddPartyDb.cs
@@ -0,0 +1,26 @@
+﻿namespace PartyProductMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddPartyDb : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Parties",
+                c => new
+                    {
+                        PartyId = c.Int(nullable: false, identity: true),
+                        PartyName = c.String(),
+                    })
+                .PrimaryKey(t => t.PartyId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Parties");
+        }
+    }
+}
